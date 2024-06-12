2eb5e47c98e9c0e33613601e92de9402460e26d7
Richard Nick Alvi Ganoza <richardalvi@yousmart.tech>
2024-06-12 13:29:22 -0500

    Add support of JWT in the Tenant Middleware

--- src/lib/middleware/tenantMiddleware.ts
+++ src/lib/middleware/tenantMiddleware.ts
@@ -1,19 +1,45 @@
 import { Request, Response, NextFunction } from "express";
 import MultiTenantKnex from "../MultiTenantKnex";
+import jwt from "jsonwebtoken";
 
-const tenantMiddleware = (multiTenantKnex: MultiTenantKnex) => {
+const tenantMiddleware = (
+  multiTenantKnex: MultiTenantKnex,
+  jwtSecret: string = ""
+) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const tenantIdHeader = req.headers["x-tenant-id"];
+    let tenantId: string | undefined;
 
-    if (!tenantIdHeader) {
-      return res.status(400).json({ error: "Missing X-Tenant-Id header" });
+    // Check for the Authorization header
+    const authHeader = req.headers["authorization"];
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      const token = authHeader.split(" ")[1];
+
+      try {
+        // Verify the token
+        const decodedToken = jwt.verify(token, jwtSecret);
+        tenantId = (decodedToken as any).tenantId;
+      } catch (error: any) {
+        return res.status(401).json({ error: "Invalid token" });
+      }
     }
 
-    const tenantId = Array.isArray(tenantIdHeader)
-      ? tenantIdHeader[0]
-      : tenantIdHeader;
+    // Check for the x-tenant-id header
+    const tenantIdHeader = req.headers["x-tenant-id"];
+    if (tenantIdHeader) {
+      tenantId = Array.isArray(tenantIdHeader)
+        ? tenantIdHeader[0]
+        : tenantIdHeader;
+    }
+
+    // Ensure a tenant ID has been determined
+    if (!tenantId) {
+      return res
+        .status(400)
+        .json({ error: "Missing X-Tenant-Id header and token tenant ID" });
+    }
 
     try {
+      // Set the tenant connection using the determined tenant ID
       await multiTenantKnex.setCurrentTenantConnection(tenantId);
     } catch (error: any) {
       return res.status(400).json({ error: error.message });
